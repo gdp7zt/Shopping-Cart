4949b2458734fcf132d00342c9b2fdd98ee693c4
Gabriel Pleimann <gdpleima02@gmail.com>
2023-01-29 13:26:52 -0500

    Attempt to fix routing

--- shopping-cart/src/components/RouteSwitch.js
+++ shopping-cart/src/components/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./home";
 import Products from "./products";
 import Contact from "./contact";
